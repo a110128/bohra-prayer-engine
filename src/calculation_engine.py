09d5dd9429f2c0b2847bfa291da00deb409a1034
a110128 <adnan.mustafa.sarkar@icloud.com>
2025-12-24 15:40:15 +0530

    Update calculation_engine.py

--- src/calculation_engine.py
+++ src/calculation_engine.py
@@ -1,1 +1,9 @@
-
+def get_prayer_times(lat, lon, date):
+    # placeholder function
+    return {
+        "fajr": "05:12",
+        "zuhr": "12:23",
+        "asr": "15:48",
+        "maghrib": "18:11",
+        "isha": "19:25"
+    }
